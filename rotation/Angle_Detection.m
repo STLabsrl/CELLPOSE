5660c8b700e4be2ddc47bcf651f07bda125a85f5
Dario Sanalitro <dariosanalitro@hotmail.it>
2025-03-06 12:00:24 +0100

    adding possibility to run the whole experiment analysis, rather than just small sections of the samples

--- rotation/Angle_Detection.m
+++ rotation/Angle_Detection.m
@@ -3,15 +3,15 @@ close all;
 
 addPaths;
 
-load('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/2025_02_20_15_12_30/Cell1_data.mat');
-load('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/2025_02_20_15_12_30/workspace.mat');
+load('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/2025_03_04_20_29_20/Cell1_data.mat');
+load('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/2025_03_04_20_29_20/workspace.mat');
 
 settings.viewcell=0;
 if settings.viewcell
     visualize_cell;
 end
 
-settings.ifrefining = 0;
+settings.ifrefining = 1;
 settings.ifplot = 0;
 settings.verbose = 1;
 settings.RadiusCells = 30;
@@ -21,7 +21,7 @@ Cell = cellData.Cell;
 
 RefinedCell = refine_cellpose(Cell, settings);
 %
-settings.extractFrequencySections = 1;
+settings.extractFrequencySections = 0;
 
 if settings.extractFrequencySections
     settings.delay = 47; % delay between one input frequency and another
@@ -31,43 +31,62 @@ if settings.extractFrequencySections
 end
 %%
 
-settings.analyzeSegments = {'30 Hz', '50 Hz'};  % Example: Analyze only '30Hz' and '50Hz'
-settings.ifMSE = 0;
+%settings.analyzeSegments = {'30 Hz', '50 Hz'};  % Example: Analyze only '30Hz' and '50Hz'
+settings.analyzeSegments = {};
+settings.ifMSE = 1;
 settings.ifCorr = 0;
-settings.ifPXI = 1;
+settings.ifPXI = 0;
 settings.ifRegionprops=1;
 
 % Loop through selected segments
-for i = 1:length(settings.analyzeSegments)
-    label = settings.analyzeSegments{i};  % Get label of the segment to analyze
+if ~isempty(settings.analyzeSegments)
+    for i = 1:length(settings.analyzeSegments)
+        label = settings.analyzeSegments{i};  % Get label of the segment to analyze
 
-    % Find the corresponding segment in CellSegments
-    idx = find(strcmp(settings.labels, label)); % Get index of the label
+        % Find the corresponding segment in CellSegments
+        idx = find(strcmp(settings.labels, label)); % Get index of the label
 
-    if isempty(idx)
-        warning('Segment %s not found in CellSegments. Skipping...', label);
-        continue;
+        if isempty(idx)
+            warning('Segment %s not found in CellSegments. Skipping...', label);
+            continue;
+        end
+
+        fprintf('Analyzing segment: %s\n', label);
+
+        % Get segment frames
+        RefinedCell = CellSegments{idx}.Frames;
+
+        % Select Rotation Estimation Method Based on Settings
+        if settings.ifMSE
+            [AngleEst, t0] = mseRotationEstimation(RefinedCell, settings);
+        elseif settings.ifCorr
+            settings.ifQuadrant = 1;
+            AngleEst = corrRotationEstimation(RefinedCell, settings);
+        elseif settings.ifPXI
+            AngleEst = pxiRotationEstimation(RefinedCell, settings);
+        elseif settings.ifRegionprops
+            AngleEst = regionpropsRotationEstimation(RefinedCell, settings);
+        else
+            warning('No valid rotation estimation method selected. Check settings.');
+            AngleEst = [];
+        end
+        % Store the angle estimation results in the struct
+        CellSegments{idx}.AngleEst = AngleEst;
     end
+else
 
-    fprintf('Analyzing segment: %s\n', label);
-
-    % Get segment frames
-    RefinedCell = CellSegments{idx}.Frames;
-
-    % Select Rotation Estimation Method Based on Settings
     if settings.ifMSE
-        AngleEst = mseRotationEstimation(RefinedCell, settings);
-    elseif settings.ifCorr
-        settings.ifQuadrant = 1;
-        AngleEst = corrRotationEstimation(RefinedCell, settings);
-    elseif settings.ifPXI
-        AngleEst = pxiRotationEstimation(RefinedCell, settings);
-    elseif settings.ifRegionprops
-        AngleEst = regionpropsRotationEstimation(RefinedCell, settings);
-    else
-        warning('No valid rotation estimation method selected. Check settings.');
-        AngleEst = [];
-    end
-    % Store the angle estimation results in the struct
-    CellSegments{idx}.AngleEst = AngleEst;
+            [AngleEst, t0] = mseRotationEstimation(RefinedCell, settings);
+        elseif settings.ifCorr
+            settings.ifQuadrant = 1;
+            AngleEst = corrRotationEstimation(RefinedCell, settings);
+        elseif settings.ifPXI
+            AngleEst = pxiRotationEstimation(RefinedCell, settings);
+        elseif settings.ifRegionprops
+            AngleEst = regionpropsRotationEstimation(RefinedCell, settings);
+        else
+            warning('No valid rotation estimation method selected. Check settings.');
+            AngleEst = [];
+        end
+
 end
