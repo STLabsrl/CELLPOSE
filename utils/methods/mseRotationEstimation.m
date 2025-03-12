92aba24786b95158757b6f805a5ad14666b58390
Dario Sanalitro <dariosanalitro@hotmail.it>
2025-03-12 11:24:41 +0100

    removing division by 1.8 and adding parameter for the angle step

--- utils/methods/mseRotationEstimation.m
+++ utils/methods/mseRotationEstimation.m
@@ -5,17 +5,18 @@ numFrames = settings.numFrames;
 ifplot = settings.ifplot;
 % Creazione Maschera Circolare
 [N1,N2] = meshgrid(-floor(size(RefinedCell(:,:,1),1)/2):floor(size(RefinedCell(:,:,1),1)/2),-floor(size(RefinedCell(:,:,1),2)/2):floor(size(RefinedCell(:,:,1),2)/2));
-CircularMask = double((N1.^2 +N2.^2)<(RadiusCells/1.8).^2);
+CircularMask = double((N1.^2 +N2.^2)<(RadiusCells).^2);
 IM = RefinedCell(:,:,1).*CircularMask;
 if ifplot imagesc(IM); axis image; axis off; colormap gray; end
 AngleEst =  zeros(1, numFrames-1);
 Frame1 = RefinedCell(:,:,1) .* CircularMask;
 t0_idx = NaN; % Initialize t0_idx as NaN in case no valid frame is found
-for i=2:length(RefinedCell)
+angle_step = 15; 
+for i=2:size(RefinedCell, 3)-1
     Frame2 = RefinedCell(:,:,i) .* CircularMask;
     mse_fun = @(Angle) mse(Frame1, imrotate(Frame2, Angle, 'bicubic', 'crop'));
-    AngleEst(i-1) = fminbnd(mse_fun, 0, 2.5);
-    if AngleEst(i-1) > 0 && AngleEst(i-1) <= 1
+    AngleEst(i-1) = fminbnd(mse_fun, 0, angle_step);
+    if AngleEst(i-1) > 0 && AngleEst(i-1) <= angle_step
         if isnan(t0_idx)  % Only set t0_idx once
             t0_idx = i - 1;  % Store the frame index
             fprintf('Rotation starts at frame %d (AngleEst = %.3f)\n', t0_idx, AngleEst(i-1));
