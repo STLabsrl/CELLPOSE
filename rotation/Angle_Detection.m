20cf3db6893f1d36767871b2055eb3978039bf3c
Dario Sanalitro <dariosanalitro@hotmail.it>
2025-03-14 19:10:07 +0100

    adding comparison and mean between frames for mse

--- rotation/Angle_Detection.m
+++ rotation/Angle_Detection.m
@@ -3,15 +3,18 @@ close all;
 
 addPaths;
 
-load('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/2025_03_10_18_46_12/Cell1_data.mat');
-load('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/2025_03_10_18_46_12/workspace.mat');
+exp = '2025_03_12_16_44_52';
+cell_name = '/Cell1_data.mat';
+workspace_name = '/workspace.mat';
+load(strcat('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/',exp,cell_name));
+load(strcat('/home/dsanalit/OneDrive/Work/Software/CELLPOSE/results/',exp,workspace_name));
 
-settings.viewcell=1;
+settings.viewcell=0;
 if settings.viewcell
     visualize_cell;
 end
 
-settings.ifplot = 0;
+settings.ifplot = 1;
 settings.verbose = 1;
 if isfield(cellData, 'Settings') && isfield(cellData.Settings, 'radius')
     settings.RadiusCells = cellData.Settings.radius;
@@ -23,22 +26,23 @@ end
 Cell = cellData.Cell;
 [settings.Cellrows, settings.Cellcols, settings.numFrames] = size(Cell);
 %
-settings.extractFrequencySections = 0;
+settings.extractFrequencySections = 1;
 
 if settings.extractFrequencySections
     settings.delay = 47; % delay between one input frequency and another
-    settings.frequency_change_points = [41, 318, 594, 872; 272 548 822 1100]; % Adjust based on your data in seconds
-    settings.labels = ["30 Hz", "40 Hz", "50 Hz", "80 Hz"]; % Corresponding frequency labels
+    settings.frequency_change_points = [116, 394, 670; 340 620 799]; % Adjust based on your data in seconds
+    settings.labels = ["30 Hz", "40 Hz", "50 Hz"]; % Corresponding frequency labels
     CellSegments = extractFrequencySections(Cell, settings);
 end
 %%
 
 %settings.analyzeSegments = {'30 Hz', '50 Hz'};  % Example: Analyze only '30Hz' and '50Hz'
-settings.analyzeSegments = {};
+settings.analyzeSegments = {'30 Hz'};
 settings.ifMSE = 1;
 settings.ifCorr = 0;
 settings.ifPXI = 0;
 settings.ifRegionprops=1;
+settings.max_shift = 10;
 
 % Loop through selected segments
 if ~isempty(settings.analyzeSegments)
@@ -78,17 +82,34 @@ if ~isempty(settings.analyzeSegments)
 else
 
     if settings.ifMSE
-            [AngleEst, t0] = mseRotationEstimationComparison(Cell, settings);
-        elseif settings.ifCorr
-            settings.ifQuadrant = 1;
-            AngleEst = corrRotationEstimation(Cell, settings);
-        elseif settings.ifPXI
-            AngleEst = pxiRotationEstimation(Cell, settings);
-        elseif settings.ifRegionprops
-            AngleEst = regionpropsRotationEstimation(Cell, settings);
-        else
-            warning('No valid rotation estimation method selected. Check settings.');
-            AngleEst = [];
-        end
+        [AngleEst, t0] = mseRotationEstimationComparison(Cell, settings);
+        computeAutoCorrelation(AngleEst,settings);
+    elseif settings.ifCorr
+        settings.ifQuadrant = 1;
+        AngleEst = corrRotationEstimation(Cell, settings);
+    elseif settings.ifPXI
+        AngleEst = pxiRotationEstimation(Cell, settings);
+        computeAutoCorrelation(AngleEst,settings);
+    elseif settings.ifRegionprops
+        AngleEst = regionpropsRotationEstimation(Cell, settings);
+    else
+        warning('No valid rotation estimation method selected. Check settings.');
+        AngleEst = [];
+    end
 
 end
+
+if ~isempty(AngleEst)
+    % Extract rotation values from AngleEst
+    allRotations = cellfun(@(x) x.Rotation, AngleEst); 
+
+    % Compute the mean rotation, ignoring NaNs
+    meanRotation = mean(allRotations, 'omitnan');
+
+    % Display the result
+    disp(['Mean Rotation: ', num2str(meanRotation)]);
+else
+    disp('AngleEst is empty. No rotation values found.');
+end
+
+
